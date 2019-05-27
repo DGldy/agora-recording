778792e4096af6a6930ae76b7d93d690f0c26c4f
David Gilady <david.g@qualisystems.local>
2019-05-27 14:19:13 +0300

    listen on port 3000 instead of 80

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 const fs = require('fs')
 const RecordManager = require('./recordManager')
 const bodyParser = require('body-parser')
